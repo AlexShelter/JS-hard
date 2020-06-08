abaa289c52d05b83f075546bc8f54eccf3ea76a2
AlexShelter <allinshelter@gmail.com>
2020-06-08 22:34:02 +0400

    refact: clear main.js to hard task

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-let money = 300000;
-console.log(typeof money);
-
-let income = 'freelance';
-console.log(typeof income);
-
-let addExpenses = 'Internet, Taxi, Phone';
-console.log(addExpenses.length);
-
-let deposit = false;
-console.log(typeof deposit);
-
-let mission = 90000000;
-let period = 1;
-
-console.log('Период равен ' + period + ' месяцев. ' + 'Цель заработать ' + mission +
- ' долларов.');
-
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
-let budgetDay = money/30;
-console.log(budgetDay);
-
 // УСЛОЖНЕННОЕ
 
 let num = 266219;
